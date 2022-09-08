faad64f3170d3b80be6cc93f95e9966cda5911c6
alrus2797 <alrus2797@gmail.com>
2022-09-08 09:00:58 -0500

    Add Header and Content components

---
+++ src/components/content/ContentComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './ContentStyles.css'
+function ContentComponent(){
+	return <div className="content container">		
+		Main Content
+	</div>
+}
+
+export default ContentComponent
---
+++ src/components/header/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HeaderStyles.css'
+
+function HeaderComponent(){
+    return <header className='header'>
+        Hola
+    </header>
+
+}
+
+export default HeaderComponent;
