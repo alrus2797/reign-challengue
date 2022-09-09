534465fdcff87f8d2651d9eccc96586c994a4eef
alrus2797 <alrus2797@gmail.com>
2022-09-08 22:48:31 -0500

    Add Tab components to Content

--- src/components/content/ContentComponent.tsx
+++ src/components/content/ContentComponent.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import './ContentStyles.css'
-function ContentComponent(){
-	return <div className="content container">		
-		Main Content
-	</div>
-}
+import React from "react";
+import RowComponent from "../row/RowComponent";
+import TabComponent from "../tabs/TabComponent";
+import "./ContentStyles.css";
 
-export default ContentComponent
+export default function ContentComponent() {
+  const [activeTab, setActiveTab] = React.useState("All");
+
+  const tabs = ["All", "Favs"];
+
+  const handleTabClick = (index: string) => {
+    console.log(index);
+    setActiveTab(index);
+  };
+  return (
+    <div className="content">
+      <RowComponent className="center">
+        <TabComponent
+          clickHandler={handleTabClick}
+          tabs={tabs}
+          activeTab={activeTab}
+        />
+      </RowComponent>
+    </div>
+  );
+}
---
+++ src/components/tabs/TabComponent.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./TabStyles.css";
+
+interface TabComponentProps {
+  clickHandler: (index: string) => void;
+  tabs: string[];
+  activeTab: string;
+}
+
+export default function TabComponent(props: TabComponentProps) {
+  const setActiveTab = (e: React.MouseEvent<HTMLDivElement>) => {
+    // console.log("Children", e.currentTarget.innerText)
+    e.currentTarget.classList.add("active");
+    props.clickHandler(e.currentTarget.innerText);
+  };
+
+  return (
+    <div className="tabs">
+      {props.tabs.map((tab, index) => (
+        <div
+          key={index}
+          className={`tab ${tab === props.activeTab ? "active" : ""}`}
+          onClick={setActiveTab}
+        >
+          <span className="Text-Style-2">{tab}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
